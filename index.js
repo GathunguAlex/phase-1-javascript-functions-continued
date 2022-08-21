0c3c573546a4c47ed6e9212696bcf0b292220c07
Alex Gathungu <alex.gathungu@student.moringaschool.com>
2022-08-21 10:23:46 +0300

    completed assignment

--- index.js
+++ index.js
@@ -4,4 +4,10 @@ function saturdayFun(activity='roller-skate')
  console.log(`This Saturday, I want to ${activity}`);
  return;
 } 
-saturdayFun();
+//saturdayFun();
+
+function mondayWork(activity = 'go to the office') {
+ return console.log(`This Monday, I will ${activity}`);
+}
+mondayWork()
+
